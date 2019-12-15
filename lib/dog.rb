6608ff0a595946df8ea36fead1c21bc13abbc9ff
Benjamin Horns <benhorns@gmail.com>
2019-12-15 08:19:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,31 @@
 # Your code goes here!
 class Dog
 
-def initialize 
-
+def initialize(name, bark)
+  @name = name
+  @bark = bark
 end
+def name=(name)
+end
+
+def name
+  @name
+end
+
+def bark=(bark)
+  @breed = breed
+end
+
+def bark
+  @breed
+end
+end
+
+fido = Dog.new
+fido.name = "Fido"
+
+fido.name
+# => "Fido"
+
+fido.bark
+woof!
