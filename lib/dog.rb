f8ae1891beb956cc128e8d0c4edc6e14bdc1de3b
Benjamin Horns <benhorns@gmail.com>
2019-12-15 08:51:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,22 +5,6 @@ class Dog
     @name, @bark = name, bark
   end
 
-  def name=(name)
-    @name = name
-  end
-
-  def bark=(bark)
-    @bark = bark
-  end
-
-  def name
-    @name
-  end
-
-def bark
-  @bark
-end
-
 
 end
 
