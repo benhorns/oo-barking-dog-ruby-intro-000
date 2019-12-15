b87d89c598743f0737395af849671f96a25ec8bf
Benjamin Horns <benhorns@gmail.com>
2019-12-15 08:42:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,29 +1,2 @@
 # Your code goes here!
 class Dog
-def initialize(bark)
-  @name = name
-  @bark = bark
-end
-
-def bark=(bark)
-  @bark = bark
-end
-
-def bark
-  @bark
-end
-
-def name
-  @name
-
-def name=(name)
-  @name = name
-end
-
-end
-fido = Dog.new("Fido","Bark")
-fido.name_set(:@name, "Fido")
-# => "Fido"
-
-fido.bark
-woof!
