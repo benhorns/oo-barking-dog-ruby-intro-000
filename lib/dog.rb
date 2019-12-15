3869f31fe7263a5662e8b28d888072dda3ba9f37
Benjamin Horns <benhorns@gmail.com>
2019-12-15 09:16:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,14 +9,15 @@ class Dog
     @name
   end
 
-  def name=(name)
-    @name = name
+  def name=(dogs_name)
+   @name = dogs_name
   end
-
+  
   def bark
 puts "Woof!"
   end
 end
 
-fido = Dog.new ("Fido")
-
+fido = Dog.new
+fido.name = "Fido"
+fido.bark
